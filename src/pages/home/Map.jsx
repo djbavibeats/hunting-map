0a3f5100809ce976d0375021ae6bf893ad8ff6cb
djbavibeats <justin@voltcreative.com>
2024-07-31 11:10:21 -0400

    Update

--- src/pages/home/Map.jsx
+++ src/pages/home/Map.jsx
@@ -69,7 +69,7 @@ const ButtonsBlock = ({ activeScreen, setActiveScreen }) => {
             </div>
                 
             <div className="mt-[1.75px]"><p className="mb-2 text-2xl font-tungsten">
-                No Locations Near You? <a href="#" className="underline">Enter To Win Here</a>
+                No Locations Near You? <a href="https://found.ee/HappyHuntingGiveaway" target="_blank" className="underline">Enter To Win Here</a>
             </p></div> 
                 {/* <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4" viewBox="0 0 512 512">
                     <path d="M256 80C149.9 80 62.4 159.4 49.6 262c9.4-3.8 19.6-6 30.4-6c26.5 0 48 21.5 48 48V432c0 26.5-21.5 48-48 48c-44.2 0-80-35.8-80-80V384 336 288C0 146.6 114.6 32 256 32s256 114.6 256 256v48 48 16c0 44.2-35.8 80-80 80c-26.5 0-48-21.5-48-48V304c0-26.5 21.5-48 48-48c10.8 0 21 2.1 30.4 6C449.6 159.4 362.1 80 256 80z"/>
